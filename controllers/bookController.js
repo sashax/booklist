5370a3d564f2e2902c791afc0ed0db04f39578f3
sasha magee <alexander.magee@gsa.gov>
2018-04-21 17:29:15 -0700

    These changes have been lurking around and ought to be checked in.

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,13 +1,24 @@
 var bookdata = require('../src/bookdata.js');
 
+
 exports.index_get = function(req, res, next) {
     var books = bookdata.read();
     res.render('books', {booklist: books});
-  };
+};
 
 exports.book_get = function(req, res, next) {
-    //needs to be abstracted to singleton or equiv
     var books = bookdata.read();
     res.render('book', {book: books[req.params.id]});
-  }
+};   
+
+exports.book_add = function(req, res, next) {
+    var book = {};
+    book.title_author = req.params.title + ' - ' + req.params.author;
+    book.rating = req.params.rating;
+    book.blurb = req.params.blurb;
+    bookdata.write(book);
+    var book_id = books.length -1;
+    books = null;
+    book_get({params: book_id}, res);
+};
 
--- src/bookdata.js
+++ src/bookdata.js
@@ -1,11 +1,22 @@
 var fs = require('fs');
 var parse = require('csv-parse/lib/sync');
 var books = null;
+var bookfile = __dirname + "/../public/data/books2017.csv";
 
 module.exports.read = function() {
   if (!books) {
-    var data = fs.readFileSync(__dirname + "/../public/data/books2017.csv");
+    var data = fs.readFileSync(bookfile);
     books = parse(data.toString(), {columns: true});
   }
   return books;
-}
+};
+
+module.exports.write = function(book) {
+  var booktext = book.title_author + ',' + book.blurb + ',' + book.rating + '\n';
+  try{
+    fs.appendFileSync(bookfile, booktext);
+    books = null;
+  } catch (err) {
+    console.log('write book failed');
+  }
+};
